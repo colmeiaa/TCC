b41951101672ea57d64a0efae608ce5e04d60918
purpleCode <paulo_esilva@yahoo.com.br>
2020-08-29 13:15:29 -0300

    Criaçao do Site

---
+++ Site/winter/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './assets/styles/global.css';
+
+function App(){
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>Bora camping</p>
+            <button type="button">Quero Acampar</button>
+        </div>
+    );  
+}
+
+export default App;
