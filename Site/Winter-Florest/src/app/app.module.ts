e60453a6fd6241aca479946cfd98f12574614776
purpleCode <paulo_esilva@yahoo.com.br>
2020-08-29 12:52:55 -0300

    Development

--- Site/Winter-Florest/src/app/app.module.ts
+++ Site/Winter-Florest/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './Models/home/home.component';
-import { GuiaComponent } from './Models/guia/guia.component';
-import { AboutComponent } from './Models/about/about.component';
+import { HomeComponent } from './Components/home/home.component';
+import { GuiaComponent } from './Components/guia/guia.component';
+import { AboutComponent } from './Components/about/about.component';
 
 @NgModule({
   declarations: [
